bcc41e98103e3e5b61d2b20d3f94cfa85cfc340c
REN-22 <renzo.bastide@gmail.com>
2026-01-09 19:35:02 +0100

    goto intercepter et fonctionne seul pd comende en double

--- src/main/java/com/ren/tchatlink/TchatLinkMixinConnector.java
+++ src/main/java/com/ren/tchatlink/TchatLinkMixinConnector.java
@@ -1,7 +1,7 @@
 package com.ren.tchatlink;
 
-import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.asm.mixin.connect.IMixinConnector;
+import org.spongepowered.asm.mixin.Mixins;
 
 public class TchatLinkMixinConnector implements IMixinConnector {
     @Override
