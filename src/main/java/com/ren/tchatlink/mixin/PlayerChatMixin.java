903a6c268a4d6c003f65399b844ed3d3a6141904
REN-22 <renzo.bastide@gmail.com>
2026-01-10 01:32:40 +0100

    métode msg joueur

--- src/main/java/com/ren/tchatlink/mixin/PlayerChatMixin.java
+++ src/main/java/com/ren/tchatlink/mixin/PlayerChatMixin.java
@@ -2,24 +2,20 @@ package com.ren.tchatlink.mixin;
 
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.game.ClientboundSystemChatPacket;
+import net.minecraft.network.protocol.game.ClientboundPlayerChatPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPacketListener.class)
-public class ChatPacketMixin {
+public class PlayerChatMixin {
 
-    @Inject(method = "handleSystemChat", at = @At("HEAD"))
-    private void onSystemChat(ClientboundSystemChatPacket packet, CallbackInfo ci) {
+    @Inject(method = "handlePlayerChat", at = @At("HEAD"))
+    private void onPlayerChat(ClientboundPlayerChatPacket packet, CallbackInfo ci) {
 
-        Component content = packet.content();
-        if (content == null) return;
-
-        String raw = content.getString();
-        System.out.println("[Mixin Packet] Chat: " + raw);
-        
+        String raw = packet.body().content().toString();
+        System.out.println("[Mixin PlayerChat] " + raw);
 
         // --- FILTRES ANTI-BOUCLE ---
         if (raw.startsWith("Baritone:")) return;
@@ -28,16 +24,19 @@ public class ChatPacketMixin {
         if (raw.contains("Baritone")) return;
         if (raw.startsWith("#goto")) return;
         if (raw.contains("Baritone:")) return;
-        if (!raw.contains("> ")) return;
         // ----------------------------
+        System.out.println(raw + "a passé les filtres");
 
-        String msg = raw.substring(raw.indexOf("> ") + 2);
+        String msg = raw.substring(raw.indexOf("> ") + 1);
+        System.out.println("extrait msg: " + msg);
 
         if (!msg.startsWith("goto ")) return;
 
         try {
             String[] args = msg.split(" ");
             if (args.length != 4) return;
+
+            System.out.println("args détectés: " + args[1] + " " + args[2] + " " + args[3]);
 
             double x = Double.parseDouble(args[1]);
             double y = Double.parseDouble(args[2]);
